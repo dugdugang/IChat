21be860d05050a5d2225ad3497d3c3749d408b18
Administrator <513642676@qq.com>
2018-03-14 17:32:43 +0800

    second、

---
+++ src/main/java/net/dg/web/ichat/push/App.java
@@ -0,0 +1,19 @@
+package net.dg.web.ichat.push;
+
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import net.dg.web.ichat.push.service.AccountService;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.util.logging.Logger;
+
+public class App {
+
+    public App() {
+//        packages(AccountService.class.getPackage().getName());
+//        //json解析器
+//        register(JacksonJsonProvider.class);
+//        //日志打印
+//        register(Logger.class);
+    }
+}
